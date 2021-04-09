108cb4b18c95d87228bd6bc4693aba906b5bd5e3
Czarina Bianca Ongkingco <11908478@benilde.edu.ph>
2021-04-10 00:46:37 +0800

    Created BookingDetails class

---
+++ LuxMobile/Models/BookingDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace LuxMobile.Models
+{
+    public class BookingDetails
+    {
+        [Key]
+        public int BookingNo { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public int ServicesAvailed { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public decimal TotalPrice { get; set; }
+
+    }
+}
