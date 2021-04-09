b5c6a6b604a734ab26dba95fa871591bc65c669d
Czarina Bianca Ongkingco <11908478@benilde.edu.ph>
2021-04-10 00:47:35 +0800

    Created Barber class

---
+++ LuxMobile/Models/Barber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace LuxMobile.Models
+{
+    public class Barber
+    {
+        [Key]
+        public int BarberNo { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public string BarberName { get; set; }
+    }
+}
